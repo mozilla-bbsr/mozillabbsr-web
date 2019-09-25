07326d85656e6996f407483eba5de5794d0b59e7
Saswata Mukherjee <saswataminsta@yahoo.com>
2019-09-26 00:44:15 +0530

    Initial Commit

--- moz-web/src/components/layout/Footer.js
+++ moz-web/src/components/layout/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
             <footer class="bg-light pb-5">
                 <div class="container text-center">
-                <p class="font-italic text-muted mb-0">&copy; Mozilla 2019</p>
+                <p class="font-italic text-muted mb-0">&copy;Mozilla 2019</p>
                 </div>
             </footer>
 
